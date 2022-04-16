88f8b06239f3e3ba144e783ceb29e85d7bfce69b
Mate Kis-Kiraly <Mate.Kis-Kiraly@Schroders.com>
2022-04-17 00:14:26 +0200

    test change

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default (req: VercelRequest, res: VercelResponse) => {
-  return res.json({ minAppVersion: "1.1" });
+  return res.json({ minAppVersion: "1.1", platfom: "ios" });
 };
