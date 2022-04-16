41f5e234bcda2d84429fba785f8f8a4bf0577855
Mate Kis-Kiraly <Mate.Kis-Kiraly@Schroders.com>
2022-04-17 00:20:56 +0200

    revert temp change

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default (req: VercelRequest, res: VercelResponse) => {
-  return res.json({ minAppVersion: "1.1", platfom: "ios" });
+  return res.json({ minAppVersion: "1.1" });
 };
